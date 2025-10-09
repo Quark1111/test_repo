06a689f3a5bc7b31111695a69e66ba6c9007e4f8
Quark1111 <tevrukovila@gmail.com>
2025-10-09 23:37:22 +0300

    Update euclid.py

--- src/euclid.py
+++ src/euclid.py
@@ -8,19 +8,19 @@ def gcd(a, b, x, y):
     while b != 0:
         g = max(a, b) // min(a, b)
         if cnt % 2 == 0:
-            ms.append([a1 - g * a2, b1 - g * b2])
-            a1 = a1 - g * a2
-            b1 = b1 - g * b2
+            ms.append([a1 - g*a2, b1 - g*b2])
+            a1 = a1 - g*a2
+            b1 = b1 - g*b2
         else:
-            ms.append([a2 - g * a1, b2 - g * b1])
-            a2 = a2 - g * a1
-            b2 = b2 - g * b1
+            ms.append([a2 - g*a1, b2 - g*b1])
+            a2 = a2 - g*a1
+            b2 = b2 - g*b1
         a, b = b, a % b
         cnt += 1
-    
+
     if ms[-2][1] < 0:
         ms[-2][0], ms[-2][1] = ms[-2][1], ms[-2][0]
-    if x * ms[-2][0] + y * ms[-2][1] != a:
+    if x*ms[-2][0] + y*ms[-2][1] != a:
         x, y = y, x
     print(ms[-2][0], '*', x, '+', ms[-2][1], '*', y, '=', a)
 ms=list(map(int, input().split()))
