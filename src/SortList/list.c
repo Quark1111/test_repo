aba2a49697be47aa36e1c42d9e8df08ba58abb63
Quark1111 <tevrukovila@gmail.com>
2025-11-24 19:34:27 +0300

    Update list.c

--- src/SortList/list.c
+++ src/SortList/list.c
@@ -9,53 +9,53 @@ void append(SortList *begin, int value){
     bool flag = 0;
     NewElem->value = value;
     while (p->next != NULL){
-	if (p->next->value > value){
-	    NewElem->next = p->next;
-	    p->next = NewElem;
-	    flag = 1;
-	    break;
-	}
-	p = p->next;
+		if (p->next->value > value){
+		    NewElem->next = p->next;
+		    p->next = NewElem;
+		    flag = 1;
+		    break;
+		}
+		p = p->next;
     }
     if (!flag){
-	NewElem->next = p->next;
-	p->next = NewElem;
+		NewElem->next = p->next;
+		p->next = NewElem;
     }
 }
 int get(SortList *begin){
     if (begin->next != NULL)
-	return begin->next->value;
+		return begin->next->value;
     return -1;
 }
 void DelElem(SortList *begin, int value){
     SortList *p = begin;
     bool flag  = 0;
     while (p->next != NULL){
-	if (p->next->value == value){
-	    SortList *elem = p->next;
-	    p->next = elem->next;
-	    flag = 1;
-	    free(elem);
-	    break;
-	}
-	p = p->next;
+		if (p->next->value == value){
+		    SortList *elem = p->next;
+		    p->next = elem->next;
+		    flag = 1;
+		    free(elem);
+		    break;
+		}
+		p = p->next;
     }
     if (!flag){
-	printf("\n %s", "the item is not in the list");
+		printf("\n %s", "the item is not in the list");
     }
 }
 void DelList(SortList *begin){
     while (begin->next != NULL){
-	SortList *p = begin->next;
-	begin->next = p->next;
-	free(p);
+		SortList *p = begin->next;
+		begin->next = p->next;
+		free(p);
     }
 }
 void PrintfList(SortList *begin){
     SortList *p = begin->next;
     while (p != NULL){
-	printf("%d %c", p->value, ' ');
-	p = p->next;
+		printf("%d %c", p->value, ' ');
+		p = p->next;
     }
     printf("\n");
 }
