0686669b284cd112914722d722396d17daf5024c
Quark1111 <tevrukovila@gmail.com>
2025-12-06 21:02:23 +0300

    Update task5.c

--- src/task5.c
+++ src/task5.c
@@ -6,20 +6,22 @@ int main()
     scanf("%s", mas);
     int cnt = 0;
     bool f = 0;
-    printf("\n");
-    for (int i = 0; i < 100; i++)
-    {
-	if (mas[i] == '\0') break;
-	if (mas[i] == '(') cnt++;
-	else if (mas[i] == ')') cnt--;
-	if (cnt < 0)
-	{
-	    printf("No");
-	    f = 1;
-	    break;
-	}
+    for (int i = 0; i < 100; i++) {
+		if (mas[i] == '\0') 
+			break;
+		if (mas[i] == '(') 
+			cnt++;
+		else if 
+			(mas[i] == ')') cnt--;
+		if (cnt < 0) {
+		    printf("not correct");
+		    f = 1;
+		    break;
+		}
     }
-    if (cnt==0) printf("Yes");
-    else if (!f) printf("No");
+    if (cnt==0) 
+		printf("correct");
+    else if (!f) 
+		printf("not correct");
     return 0;
 }
