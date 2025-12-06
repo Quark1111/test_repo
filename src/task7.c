c522bebafae8e6fe1b7f7e693946980b66e62420
Quark1111 <tevrukovila@gmail.com>
2025-12-06 21:07:31 +0300

    Update task7.c

--- src/task7.c
+++ src/task7.c
@@ -1,19 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
     int mas[1000];
     int n;
     scanf("%d", &n);
-    for (int i = 0; i < n; i++)
-    {
-	int c;
-	scanf("%d", &c);
-	mas[i] = c;
+    for (int i = 0; i < n; i++) {
+		int c;
+		scanf("%d", &c);
+		mas[i] = c;
     }
     int cnt = 0;
-    for (int i = 0; i < n;  i++)
-    {
-	if (mas[i] == 0) cnt++;
+    for (int i = 0; i < n;  i++) {
+		if (mas[i] == 0) 
+			cnt++;
     }
     printf("%d \n", cnt);
     return 0;
