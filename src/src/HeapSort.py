0d985f85085dc0bb0237ead544cc0135434ea0d0
Ilya Tevrukov <tevrukovila@gmail.com>
2025-12-12 16:37:25 +0000

    add HeapSort and Test_heap_sort

---
+++ src/src/HeapSort.py
@@ -0,0 +1,26 @@
+def heapify(mas, n, i):
+        largest = i
+        left = i*2 + 1
+        right = i*2 + 2
+
+        if left < n and mas[largest] < mas[left]:
+            largest = left
+        if right < n and mas[largest] < mas[right]:
+            largest = right
+
+        if largest != i:
+            mas[i], mas[largest] = mas[largest], mas[i]
+            heapify(mas, n, largest)
+def HeapSort(mas):
+    n = len(mas)
+    
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(mas, n, i)
+
+    for i in range(n - 1,  -1, -1):
+        mas[0], mas[i] = mas[i], mas[0]
+        heapify(mas, i, 0)
+
+    return mas
+
+print(HeapSort([21, 1, 678, 12, 0, 18, 19]))
