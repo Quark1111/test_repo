4e50987a8c7549662ff24938a8c6b5c648a0ce5d
Quark1111 <tevrukovila@gmail.com>
2025-12-23 23:04:51 +0300

    Update sort.c

--- src/example/sort.c
+++ src/example/sort.c
@@ -3,12 +3,12 @@
 void sort(int *arr, int len)
 {
     for (int i = 0; i < len; i++) {
-	for (int j = i + 1; j < len; j++) {
-	    if (arr[i] > arr[j]) {
-		int tmp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = tmp;
-	    }
-	}
+		for (int j = i + 1; j < len; j++) {
+		    if (arr[i] > arr[j]) {
+				int tmp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = tmp;
+		    }
+		}
     }
 }
