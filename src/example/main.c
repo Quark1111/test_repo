1a4dca34599c68019a28b5f3eded252ca21d04a6
Quark1111 <tevrukovila@gmail.com>
2025-12-23 23:03:58 +0300

    Update main.c

--- src/example/main.c
+++ src/example/main.c
@@ -5,8 +5,8 @@ void readNumber(int *arr, int *len)
 {
     int number;
     while (scanf("%d", &number) == 1 && (*len) < 100) {
-	arr[*len] = number;
-	(*len)++;
+		arr[*len] = number;
+		(*len)++;
     }
 }
 
@@ -14,15 +14,14 @@ int main()
 {
     int arr[100], arrInitial[100], len = 0, changed = 0;
     readNumber(arrInitial, &len);
-    for (int i = 0; i < len; i++) {
-	arr[i] = arrInitial[i];
-    }
+    for (int i = 0; i < len; i++)
+		arr[i] = arrInitial[i];
     sort(arr, len);
     for (int i = 0; i < len; i++) {
-	if (arr[i] != arrInitial[i])
-	    changed++;
+		if (arr[i] != arrInitial[i])
+	    	changed++;
     }
     for (int i = 0; i < len; i++)
-	printf("%d %c", arr[i], ' ');
+		printf("%d %c", arr[i], ' ');
     return changed;
 }
