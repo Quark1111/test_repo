9f8554d023fade8ba4987753b21eadcdaeb0e0d1
Quark1111 <tevrukovila@gmail.com>
2025-12-24 03:17:17 +0300

    Update lucky_tickets.c

--- src/example5/lucky_tickets.c
+++ src/example5/lucky_tickets.c
@@ -5,15 +5,15 @@ int main()
     int sums[28], res = 0;
     
     for (int i = 0; i < 28; i++)
-	sums[i] = 0;
+		sums[i] = 0;
 
     for (int a = 0; a <= 9; a++)
-	for (int b = 0; b <= 9; b++)
-	    for (int c = 0; c <= 9; c++)
-		sums[a + b + c] += 1;
+		for (int b = 0; b <= 9; b++)
+	    	for (int c = 0; c <= 9; c++)
+				sums[a + b + c] += 1;
 
     for (int i = 0; i < 28; i++)
-	res += sums[i] * sums[i];
+		res += sums[i] * sums[i];
     
     printf("%d", res);
     return 0;
