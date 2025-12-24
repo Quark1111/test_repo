badc6913367a057b91b03568fc86bfd97a3fb4ef
Quark1111 <tevrukovila@gmail.com>
2025-12-24 04:47:22 +0300

    Update division.c

--- src/example4/division.c
+++ src/example4/division.c
@@ -6,22 +6,22 @@ bool Div(int a, int b, int* result)
 {
     int absA = abs(a), absB = abs(b);
     if (b == 0)
-	return 0;
+		return 0;
 
     if (a == b)
-	return 1;
+		return 1;
     
     if (a == 0)
-	return 1;
+		return 1;
 
     while (absA >= absB) {
-	absA -= absB;
-	(*result)++;
+		absA -= absB;
+		(*result)++;
     }
     if (a * b < 0) {
-	if (absA)
-	    (*result)++;
-	(*result) *= -1;
+		if (absA)
+	    	(*result)++;
+		(*result) *= -1;
     }
 
     return 1;
@@ -32,8 +32,8 @@ int main()
     int a,b, result = 0;
     scanf("%d %d", &a, &b);
     if (Div(a, b, &result)) {
-	printf("%d %d %d", a, b, result);
+		printf("%d %d %d", a, b, result);
     } else 
-	printf("%s", "impossible to divide");
+		printf("%s", "impossible to divide");
     return 0;
 }
