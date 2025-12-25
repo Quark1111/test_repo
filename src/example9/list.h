08d8445a4920d2a75f949032ba8f6d006ca7494c
Quark1111 <tevrukovila@gmail.com>
2025-12-25 18:17:22 +0300

    Update list.h

--- src/example9/list.h
+++ src/example9/list.h
@@ -1,10 +1,10 @@
 typedef struct SortList {
     int value;
-    struct SortList *next;
+    struct SortList* next;
 } SortList;
 
-void append(SortList *begin, int value);
-int get(SortList *begin);
-void DelElem(SortList *begin, int value);
-void DelList(SortList *begin);
-void PrintfList(SortList *begin);
+void append(SortList* begin, int value);
+int get(SortList* begin);
+void DelElem(SortList* begin, int value);
+void DelList(SortList* begin);
+void PrintfList(SortList* begin);
